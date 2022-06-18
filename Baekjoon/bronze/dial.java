bde55a87185c75415ae089fb0c8ebef45c373e97
JuSeong1130 <jsmoon1130@naver.com>
2022-06-19 01:06:42 +0900

    테스트

---
+++ Baekjoon/bronze/dial.java
@@ -0,0 +1,13 @@
+package Baekjoon.bronze;
+
+import java.util.Scanner;
+//https://www.acmicpc.net/submit/5622/30501867
+public class dial {
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        String s = sc.nextLine();
+        int count=0;
+
+    }
+}
